a863c87b5e1e0767eb5ec4d9e9751a21eb529fbb
Punit2002 <punitgupta263@gmail.com>
2024-11-13 12:27:25 +0530

    first commit

--- src/components/tournaments.js
+++ src/components/tournaments.js
@@ -9,6 +9,7 @@ function Tournament() {
     const fetchTournaments = async () => {
       const response = await axios.get('https://lichess.org/api/tournament');
       setTournaments(response.data.created);
+      console.log(response);
     };
     
     fetchTournaments();
@@ -21,7 +22,7 @@ function Tournament() {
         {tournaments.map((tournament) => (
           <li key={tournament.id} className="list-item">
             <p className="tournament-info">
-              <span className="title">Name: </span>{tournament.name}
+              <span className="title">Name: </span>{tournament.fullName}
             </p>
             <p className="tournament-info">
               <span className="title">Type: </span>{tournament.rated ? 'Rated' : 'Casual'}
